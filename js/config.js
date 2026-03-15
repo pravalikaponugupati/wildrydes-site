45bbd059aa3ac53665a8c7937775157e5459fec8
pravalikaponugupati <103594167+pravalikaponugupati@users.noreply.github.com>
2026-03-15 09:51:53 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_AJIVYTekc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7mu2i5u8kq05qmk51gu4bfo70s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
